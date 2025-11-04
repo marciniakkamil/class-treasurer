6ff2bd2ff2f97d8f94a0f366b7f78eb9aa693ef6
Kamil Marciniak <kamil.marciniak@johncube.pl>
2025-11-04 21:15:51 +0100

    Test configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // In the test environment, bypass CSRF to simplify HTTP feature tests
+        if (app()->environment('testing')) {
+            $middleware->validateCsrfTokens(except: ['*']);
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
